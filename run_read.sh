413e6ff49d672472c3ef83682ec87e43e31f50b8
Thanh Phu <git@thanhphu.net>
2020-02-04 20:36:08 +0900

    Increase to 16k value

--- run_read.sh
+++ run_read.sh
@@ -3,8 +3,8 @@
 export GOMAXPROCS=128
 export LD_LIBRARY_PATH=/usr/local/lib
 
-go test --bench BenchmarkReadRandomBadger --keys_mil 1 --valsz 16384 --dir data --timeout 10m --benchtime 1m -v
-go test --bench BenchmarkReadRandomRocks --keys_mil 1 --valsz 16384 --dir data --timeout 10m --benchtime 1m -v
-go test --bench BenchmarkReadRandomBolt --keys_mil 1 --valsz 16384 --dir data --timeout 10m --benchtime 1m -v
-go test --bench BenchmarkReadRandomLevel --keys_mil 1 --valsz 16384 --dir data --timeout 10m --benchtime 1m -v
-go test --bench BenchmarkReadRandomLmdb --keys_mil 1 --valsz 16384 --dir data --timeout 10m --benchtime 1m -v
+go test --bench BenchmarkReadRandomBadger --keys_mil 1 --valsz 16384 --dir data --timeout 1m --benchtime 1m -v
+go test --bench BenchmarkReadRandomRocks --keys_mil 1 --valsz 16384 --dir data --timeout 1m --benchtime 1m -v
+go test --bench BenchmarkReadRandomBolt --keys_mil 1 --valsz 16384 --dir data --timeout 1m --benchtime 1m -v
+go test --bench BenchmarkReadRandomLevel --keys_mil 1 --valsz 16384 --dir data --timeout 1m --benchtime 1m -v
+go test --bench BenchmarkReadRandomLmdb --keys_mil 1 --valsz 16384 --dir data --timeout 1m --benchtime 1m -v
